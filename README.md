d7012cad8b76a90fe25223f15a061b258f1a409c
Frank Wiles <frank@revsys.com>
2019-09-29 17:07:51 -0500

    Add basic setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hello Docker
+
+Simple way to test if a student's local Docker setup is working correctly.
+
---
+++ src/js/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <h1 className="text-5xl bg-blue-600 border border-blue-700 text-white p-2 my-4">
+            Hello Docker!
+        </h1>
+    )
+}
+
+export default Header;
